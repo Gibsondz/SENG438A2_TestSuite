728d3c3def1291d521a3114f6bcece62a659228e
Evan Fenton <evanf347@gmail.com>
2019-03-13 11:54:58 -0600

    moved expand and equals tests to updated modern test suite

--- Updated Modern Test Suite 3.9.2019/ExpandCoverageTests.java
+++ Updated Modern Test Suite 3.9.2019/ExpandCoverageTests.java
@@ -2,6 +2,7 @@ package org.jfree.data;
 
 import static org.junit.Assert.*;
 
+import org.jfree.data.Range;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -11,6 +12,7 @@ import org.junit.Test;
 public class ExpandCoverageTests {
 
 	Range range1;
+	Range range2;
 	Range nullrange;
 	
 	@BeforeClass public static void setUpBeforeClass() throws Exception
@@ -22,28 +24,45 @@ public class ExpandCoverageTests {
 	public void setUp() throws Exception
 	{
 		range1 = new Range(2,6);
-		
+		range2 = new Range(1,1);
+		nullrange = null;
 		
 	}
+	
 	//Coverage Testing for Expand
-	@Test
-	public void NullRange() {
-		try
-		{
-			Range.expand(nullrange, 1, 2);
-			fail();
-		}
-		catch(IllegalArgumentException e)
-		{
-			
-		}
+	@Test (expected = IllegalArgumentException.class)
+	public void nullRange() throws IllegalArgumentException {
+		
+		Range.expand(nullrange, 1, 2); // not sure about how the two percentage values are interpreted if they're > 1
+		
+		//fail();
+		
 	}
 	
 	@Test
 	public void expandTest()
 	{
+		// length of range is 6-2=4, so 0.25*4=1 and 0.5*4=2,
+		// which gives lower=lower-1 and upper=upper+2
 		assertEquals(Range.expand(range1, 0.25,0.5), new Range(1,8));
 	}
+	
+	@Test 
+	public void negativeMargins() {
+		
+		// shrinks the range if margin percentage is negative (lower=lower+1 and upper=upper-2)
+		assertEquals("Range returned is incorrect when 'expanded' by a negative margin (shrank)",
+				Range.expand(range1, -0.25,-0.5), new Range(3,4));
+	}
+	
+	@Test  (expected = IllegalArgumentException.class)
+	public void negMarginInvalidLower() throws IllegalArgumentException {
+		
+		// shrinks the range but lower=lower+4 and upper=upper-2 so lower>upper
+		Range.expand(range1, -1.0,-0.5);
+	}
+	
+	
 	
 	
 	@After
---
+++ Updated Modern Test Suite 3.9.2019/RangeTestEquals.java
@@ -0,0 +1,62 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+import org.jfree.data.Range;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RangeTestEquals {
+
+	Range nullRange;
+	Range rangeLower;
+	Range rangeUpper;
+	Range rangeNormal;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		nullRange = null;
+		rangeLower = new Range(1, 10);
+		rangeUpper = new Range(2, 11);
+		rangeNormal = new Range(1, 11);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test (expected = Exception.class)
+	public void nullRangeTest() throws Exception {
+		boolean result = rangeNormal.equals(nullRange);
+	}
+	
+	@Test
+	public void sameLower() {
+		assertFalse("The objects were determined to be equal even though they dont have the same upper bound",
+				rangeNormal.equals(rangeLower));
+	}
+	
+	@Test
+	public void sameUpper() {
+		assertFalse("The objects were determined to be equal even though they dont have the same lower bound",
+				rangeNormal.equals(rangeUpper));
+	}
+	
+	@Test
+	public void sameRange() {
+		assertTrue("The objects were not determined to be equal even though they have the same bounds",
+				rangeNormal.equals(rangeNormal));
+	}
+	
+
+}
