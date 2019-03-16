26e9a1a6db25b0660a4951e641b33301bbd43102
breesuisui <breesuisui@gmail.com>
2019-03-15 20:58:51 -0600

    Update CreateNumberArray2DTest.java

--- Updated Modern Test Suite 3.9.2019/CreateNumberArray2DTest.java
+++ Updated Modern Test Suite 3.9.2019/CreateNumberArray2DTest.java
@@ -1,26 +1,21 @@
-package org.jfree.data.test;
+package org.jfree.data;
+
 
 import static org.junit.Assert.*;
 
+import org.jfree.data.DataUtilities;
+import org.junit.*;
 import java.security.InvalidParameterException;
 
-import org.jfree.data.DataUtilities;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-public class CreateNumberArray2DTest {
+public class createNumberArray2DTest {
 	
 	private double [][] doubleArray, doubleArray2 = {
-			{1.0, 2.0, 3.0, 4.0},
-			{5.0, 6.0, 7.0, 8.0}
+			{1.0, 2.0, 3.0, 4.0},{5.0, 6.0, 7.0, 8.0}
 			};
 	
 	private Number [][] numberArray, numberArray2 = {
-			{1.0, 2.0, 3.0, 4.0},
-			{5.0, 6.0, 7.0, 8.0}
+			{1.0, 2.0, 3.0, 4.0},{5.0, 6.0, 7.0, 8.0}
+
 		};
 
 	@Test
@@ -34,13 +29,12 @@ public class CreateNumberArray2DTest {
 					+ "an InvalidParameterException",InvalidParameterException.class,e.getClass());
 		}
 	}
-	
 
 	@Test
 	public void createNumberArray2DWithValid2DArrayOfDoublesTest() {
 		numberArray = DataUtilities.createNumberArray2D(doubleArray2);
-		assertArrayEquals("The number 2D array created by the function does not have the same values"
-				+ " as the 2D array of doubles", numberArray2, numberArray);
+		assertArrayEquals("The numberArray2 doest not equal to"
+				+ " the doubleArray2", numberArray2, numberArray);
 	}
 
 }
